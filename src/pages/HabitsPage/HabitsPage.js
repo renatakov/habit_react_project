31b73c4de8a2caf4ba28045fe9a9c8434945a473
dan4ik58620 <danyakotkot@gmail.com>
2021-04-24 13:05:01 +0300

    1

--- src/pages/HabitsPage/HabitsPage.js
+++ src/pages/HabitsPage/HabitsPage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { Component } from 'react';
+import styles from './HabitsPage.module.scss';
 
-const HabitPage = () => {
-  return(
-    <h1>HabitPage</h1>
-  )
+class HabitPage extends Component {
+  render() {
+    return (
+      <section className={styles.habitPage}>
+        <div class={styles.top}>
+          <form action="/developHabit">
+            <button class={styles.btn}>➕</button>
+          </form>
+          <button class={styles.camera} alt="logo" />
+        </div>
+        <h1 class={styles.text}>Привычки</h1>
+        <div id="calendar" class={styles.calendar}>
+          <input type="date" name="calendar" />
+        </div>
+      </section>
+    );
+  }
 }
 
-export default HabitPage
+export default HabitPage;
