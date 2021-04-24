2fb0e9ad592011a740e8126f667cb6cc7f0a59b1
renatakov <renatakov1010@gmail.com>
2021-04-24 12:57:03 +0300

    sec commit

--- src/redux/habits/actions.js
+++ src/redux/habits/actions.js
@@ -1,3 +1,19 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction } from '@reduxjs/toolkit';
 
 // создаем экшены для привычек
+
+export const habitUpdateRequest = createAction('HABITUPDATE_REQUEST');
+export const habitAddRequest = createAction('HABITADD_REQUEST');
+export const habitDeleteRequest = createAction('HABITDELETE_REQUEST');
+export const getAllHabitsRequest = createAction("GETALLHABITS_REQUEST");
+
+export const habitUpdateSuccess = createAction('HABITUPDATE_SUCCESS');
+export const habitAddSuccess = createAction('HABITADD_SUCCESS');
+export const habitDeleteSuccess = createAction('HABITDELETE_SUCCESS');
+export const getAllHabitsSuccess = createAction("GETALLHABITS_SUCCESS");
+
+
+export const habitUpdateError = createAction('HABITUPDATE_ERROR');
+export const habitAddError = createAction('HABITADD_ERROR');
+export const habitDeleteError = createAction('HABITDELETE_ERROR');
+export const getAllHabitsError = createAction("GETALLHABITS_ERROR");
