68202dd931ef9df408dcb2c5fa5a33629232aea2
Yabl04ko23 <aloshanizi24@gmail.com>
2021-05-01 10:18:29 +0300

    create base private & public routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "modern-normalize/modern-normalize.css";
-import "./styles/_shared.scss"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize/modern-normalize.css';
+import './styles/_shared.scss';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store.store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById('root'),
 );
