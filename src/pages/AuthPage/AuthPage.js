ecc60b676f5ef23474b4f93c428725f5b2b36570
Vanya1808 <Vanya.Goudz@gmail.com>
2021-05-01 12:54:47 +0300

    hard

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,60 +1,18 @@
-import React, { Component } from 'react';
-import s from './AuthPage.module.scss';
+import logo from '../../images/logo.svg'
+import '../../App.css'
+import s from './AuthPage.module.scss'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-import { connect } from 'react-redux';
-import operations from '../../redux/auth/operations';
-
-class AuthPage extends Component {
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    // const { firstName, lastName, phoneNumber, password } = this.state;
-    // const user = {
-    //   firstName,
-    //   lastName,
-    //   phoneNumber,
-    //   password,
-    // };
-
-    const user = {
-      phoneNumber: '0934478779',
-      password: 'qwer1234',
-      firstName: 'root',
-      lastName: 'root',
-      height: 0,
-      weight: 0,
-      birthday: new Date('2021, 01, 01'),
-    };
-
-    this.props.onRegister(user);
-    console.log(user);
-  };
-  render() {
-    return (
-      <section className={s.authSection}>
-        <h1>AuthPage</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="firstName" onChange={this.handleChange} />
-          <input type="text" name="lastName" onChange={this.handleChange} />
-          <input type="text" name="phoneNumber" onChange={this.handleChange} />
-          <input type="password" name="password" onChange={this.handleChange} />
-          <button>Submit</button>
-        </form>
-      </section>
-    );
-  }
+const AuthPage = () => {
+  return (
+    <section className={s.authSection}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <NavLink to="/login" className={s.loginBtn}>Вход</NavLink>
+      <NavLink to="/register" className={s.registerBtn}>Создать аккаунт</NavLink>
+    </section>
+  )
 }
-
-const mapStateToProps = state => ({
-  state: state,
-});
-
-const mapDispatchToProps = dispatch => ({
-  onRegister: value => dispatch(operations.register(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AuthPage);
+export default AuthPage
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,52 +1,46 @@
-
 import React, { useState } from "react";
-
-
-import "./LoginPage.module.scss";
+import styles from  "./LoginPage.module.scss";
 import {useDispatch} from 'react-redux';
+import operations from '../../redux/auth/operations'
 
-
-
-
-
-export default function LoginpPage() {
-  const [values, setValues] = useState({
-    phone: ""
-  })
-
-  console.log(values.phone);
-  
-
+export default function LoginPage() {
+  const [phone, setPhone] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const [pass, setPass] = useState("");
+  const dispatch = useDispatch()
 
   const handlePhone = (event) => {
-    setValues({...values, phone: event.target.value})
+    setPhone(event.target.value);
+  }
+
+  const handlePass = (event) => {
+    setPass(event.target.value);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     
     setSubmitted(true);
+    dispatch(operations.login(phone))
+    setPhone("");
   }
-
 
   return (
     <div className='loginjs'>
   
       <form className="form-2" onSubmit={handleSubmit}>
-        {submitted ? <div className="success__message">You logged in !</div> : null}
-     
-       
+        {submitted ? <div className="success__message">You logged in !</div> : null}  
         <input
-          value={values.phone}
+          value={phone}
           type="tel"
           name="phone"
           
           onChange={handlePhone}
           required
         />
+        <input value={pass} type="password" name="pass" onChange={handlePass} required/>
         
-        {submitted && !values.phone ? <span>Пожалуйста введите ваш номер</span> : null}
+        {submitted && !phone ? <span>Пожалуйста введите ваш номер</span> : null}
         <button className="button-1" type="submit">
           Дальше
         </button>
