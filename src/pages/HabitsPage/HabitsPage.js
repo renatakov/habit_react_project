8c71f52c0c46329d7e7de779855d1b0d335d8721
dan4ik58620 <danyakotkot@gmail.com>
2021-05-08 12:05:31 +0300

    modal

--- src/pages/HabitsPage/HabitsPage.js
+++ src/pages/HabitsPage/HabitsPage.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import styles from './HabitsPage.module.scss';
+import Modal from '../../components/Modal/Modal';
+import HabitForm from '../../components/HabitForm/HabitForm';
 
 class HabitPage extends Component {
-  createHabit = () => {};
+  state = {
+    showModal: false,
+  };
+  showModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
   render() {
     return (
       <section className={styles.habitPage}>
         <div class={styles.top}>
-          <button onClick={this.createHabit} class={styles.btn}>&#43;</button>
-
+          <button onClick={this.showModal} class={styles.btn}>
+            &#43;
+          </button>
+          {this.state.showModal && (
+            <Modal toggleModal={this.showModal}>
+              <HabitForm />
+            </Modal>
+          )}
           <button class={styles.camera} alt="logo" />
         </div>
         <h1 class={styles.text}>Привычки</h1>
