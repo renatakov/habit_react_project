0673bd4f3f5ec550c300a5796b1dcb645f8ce4fc
dan4ik58620 <danyakotkot@gmail.com>
2021-05-08 12:43:32 +0300

    link

--- src/pages/HabitsPage/HabitsPage.js
+++ src/pages/HabitsPage/HabitsPage.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import styles from './HabitsPage.module.scss';
 import Modal from '../../components/Modal/Modal';
 import HabitForm from '../../components/HabitForm/HabitForm';
+import { Link } from 'react-router-dom';
 
 class HabitPage extends Component {
   state = {
     showModal: false,
+    habitsList: [
+      {
+        id: '1',
+        name: 'зарядка',
+        perfomance: '50%',
+      },
+      {
+        id: '2',
+        name: 'зарядка2',
+        perfomance: '50%',
+      },
+    ],
   };
   showModal = () => {
     this.setState({ showModal: !this.state.showModal });
@@ -28,6 +41,17 @@ class HabitPage extends Component {
         <div id="calendar" class={styles.calendar}>
           <input type="date" name="calendar" />
         </div>
+        {this.state.habitsList.length && (
+          <ul>
+            {this.state.habitsList.map(habit => {
+              return (
+                <li className={styles.habitItem} key={habit.id}>
+                  <Link to="/habitItem">{habit.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </section>
     );
   }
