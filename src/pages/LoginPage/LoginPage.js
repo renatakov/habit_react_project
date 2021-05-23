8b77523fe7405475d06130a9b3decf7b5ce9d6f2
Yabl04ko23 <aloshanizi24@gmail.com>
2021-05-23 14:37:55 +0300

    сделал готвые стили, переход назад и на главную страницы

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-// import { func } from 'prop-types';
-// import Form from './';
-// import Logo from './Logo';
-// import './LoginPage.module.scss';
-// import waves from '../../images/desktop/';
-// import logo from '../../images/logo.svg';
-// import flag from '../../images/';
-
-export default function LoginpPage() {
-  const [values, setValues] = useState({
-    phone: '',
-  });
-
-  // console.log(values.phone);
-=======
-import React, { useState } from "react";
-import styles from  "./LoginPage.module.scss";
-import {useDispatch} from 'react-redux';
 import operations from '../../redux/auth/operations'
->>>>>>> af162f7accb04b73d86367d9198ef04935c7a62f
 
 export default function LoginPage() {
   const [phone, setPhone] = useState("");
@@ -28,11 +8,6 @@ export default function LoginPage() {
   const [pass, setPass] = useState("");
   const dispatch = useDispatch()
 
-<<<<<<< HEAD
-  const handlePhone = event => {
-    setValues({ ...values, phone: event.target.value });
-  };
-=======
   const handlePhone = (event) => {
     setPhone(event.target.value);
   }
@@ -40,26 +15,9 @@ export default function LoginPage() {
   const handlePass = (event) => {
     setPass(event.target.value);
   }
->>>>>>> af162f7accb04b73d86367d9198ef04935c7a62f
 
   const handleSubmit = event => {
     event.preventDefault();
-<<<<<<< HEAD
-
-    setSubmitted(true);
-  };
-
-  return (
-    <div className="loginjs">
-      {/* <img src={logo} /> */}
-      <form className="form-2" onSubmit={handleSubmit}>
-        {submitted ? (
-          <div className="success__message">You logged in !</div>
-        ) : null}
-        {/* <img src={flag} /> */}
-
-=======
-    
     setSubmitted(true);
     dispatch(operations.login(phone))
     setPhone("");
@@ -67,10 +25,8 @@ export default function LoginPage() {
 
   return (
     <div className='loginjs'>
-  
       <form className="form-2" onSubmit={handleSubmit}>
-        {submitted ? <div className="success__message">You logged in !</div> : null}  
->>>>>>> af162f7accb04b73d86367d9198ef04935c7a62f
+        {submitted ? <div className="success__message">You logged in !</div> : null}
         <input
           value={phone}
           type="tel"
@@ -78,25 +34,12 @@ export default function LoginPage() {
           onChange={handlePhone}
           required
         />
-<<<<<<< HEAD
-
-        {submitted && !values.phone ? (
-          <span>Пожалуйста введите ваш номер</span>
-        ) : null}
-=======
         <input value={pass} type="password" name="pass" onChange={handlePass} required/>
-        
         {submitted && !phone ? <span>Пожалуйста введите ваш номер</span> : null}
->>>>>>> af162f7accb04b73d86367d9198ef04935c7a62f
         <button className="button-1" type="submit">
           Дальше
         </button>
       </form>
-<<<<<<< HEAD
-      {/* <img className="wawes" src={waves} /> */}
-=======
-     
->>>>>>> af162f7accb04b73d86367d9198ef04935c7a62f
     </div>
   );
 }
--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React from 'react';
+import styles from './UserPage.module.scss';
+import { NavLink } from 'react-router-dom';
+
+const getFile = event => {
+  // let files = event.target.files,
+  //   filename = files[0].name,
+  //   fr = new FileReader();
+  //   fr.onload = function () {
+  //     console.log(fr.result);
+  //     document.querySelector(".avatarImage").src = fr.result;
+  // }
+  // fr.readAsDataURL(files[0]);
+};
 
 const UserPage = () => {
-  return(
-    <h1>UserPage</h1>
-  )
-}
+  return (
+    <>
+      <header className={styles.userFormHeader}>
+        <NavLink to="/">
+          <div className={styles.goBack}>
+            <span className={styles.goBackArrow}>&#706;</span>
+            <span>Назад</span>
+          </div>
+        </NavLink>
+        <div className={styles.userPhoto}>
+          <input
+            id={styles.photoUpload}
+            className={styles.uploadInput}
+            type="file"
+            accept="image/*"
+            onChange={event => getFile(event)}
+          />
+          <div className={styles.avatarImage}></div>
+          <span className={styles.headerPhotoText}>Сменить фото</span>
+        </div>
+      </header>
+
+      <div>
+        <form className={styles.userForm}>
+          <div className={styles.inputsData}>
+            <div className={styles.dataGroup}>
+              <span className={styles.formInputsText}>Рост*</span>
+              <input
+                className={styles.formInputs}
+                id={styles.growthInput}
+                type="number"
+                placeholder="170 см"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className={styles.dataGroup}>
+              <span className={styles.formInputsText}>Вес*</span>
+              <input
+                className={styles.formInputs}
+                id={styles.weightInput}
+                type="number"
+                placeholder="50 кг"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className={styles.dataGroup}>
+              <span className={styles.formInputsText}>Дата Рождения*</span>
+              <input
+                className={styles.formInputs}
+                id={styles.birthInput}
+                type="date"
+                placeholder="25. 09. 1986"
+                required
+                autoComplete="off"
+              />
+            </div>
+          </div>
+          <NavLink to="/habits">
+            <button className={styles.createBtn}>Создать</button>
+          </NavLink>
+        </form>
+        <div className={styles.footer}></div>
+      </div>
+    </>
+  );
+};
 
 export default UserPage;
